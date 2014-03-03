7230817e8c3b9c62dd86559174caf1f3b6679eca
David Lawrence Miller <github@ninepointeightone.net>
2014-03-03 16:24:54 -0500

    clarified build process

--- dsm/build-book.r
+++ dsm/build-book.r
@@ -4,11 +4,17 @@
 library(devtools)
 source("rmd2html.r")
 
+# what Hadley calls "chapters" we might call "sections"...
 chapters <- dir(".", pattern = "\\.Rmd$")
 
+# remove the old markdown files
+system("rm markeddown/dsm-*")
+
+# Rmd -> md with custom knitr
 for (chapter in chapters) {
   out <- cache_file(chapter, rmd2md, ".md")
   file.copy(out, paste0("markeddown/", gsub(".Rmd", ".md", chapter)))
 }
 
+# actually call pandoc
 system("sh build.sh")
